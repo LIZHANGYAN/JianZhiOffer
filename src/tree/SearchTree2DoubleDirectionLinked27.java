add5a265bbd390661925bb0626cf30b012e151fc
LIZHANGYAN <lizhangyan2013@live.com>
2018-06-19 14:01:13 +0800

    题目27

--- src/tree/SearchTree2DoubleDirectionLinked27.java
+++ src/tree/SearchTree2DoubleDirectionLinked27.java
@@ -8,9 +8,32 @@ import java.util.Scanner;
 public class SearchTree2DoubleDirectionLinked27 {
 
     private static TreeNode Convert(TreeNode pRootOfTree){
-        TreeNode result = null;
-
-        return result;
+        if(pRootOfTree == null){
+            return null;
+        }
+        TreeNode headNode;
+        headNode = ConvertNode(pRootOfTree, null);
+        while(headNode !=null && headNode.left != null)
+            headNode = headNode.left;
+        return headNode;
+    }
+    private static TreeNode ConvertNode(TreeNode pNode, TreeNode lastNode){
+        if(pNode == null){
+            return lastNode;
+        }
+        TreeNode current = pNode;
+        if(current.left != null){
+            lastNode = ConvertNode(current.left, lastNode);
+        }
+        current.left = lastNode;
+        if(lastNode != null){
+            lastNode.right = current;
+        }
+        lastNode = current;
+        if(current.right != null){
+            lastNode = ConvertNode(current.right, lastNode);
+        }
+        return lastNode;
     }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -21,8 +44,9 @@ public class SearchTree2DoubleDirectionLinked27 {
                 treenodes[i] = in.nextInt();
             }
             TreeNode root = TreeNode.createBinaryTreeByArray(treenodes, 0);
-            System.out.println("hhh");
 
+            TreeNode root2 = Convert(root);
+            System.out.printf("hhh");
 
         }
     }
